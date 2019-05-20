f9c4826b2815d21055911fabf013632932a3b96e
yuyang <18210033490@163.com>
2019-05-20 14:06:47 +0800

    vscode debugger in chrome 对vue-cli3 配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="getES6Feature(`进入debugger`)">设置断点</button>
+    <button @click="getES6Feature(`加载断点`)">加载断点</button>
   </div>
 </template>
 
@@ -13,14 +15,20 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 export default Vue.extend({
   name: "home",
   methods: {
-    getES6Feature() {
-      //* 小明
+    getES6Feature (message:string) {
+      if (message) {
+          console.log(message);
+          return ;
+      }
 
       console.log(`I am VScode`);
-    }
+    },
   },
   components: {
     HelloWorld
-  }
+  },
+  created () {
+    //   this.getES6Feature();
+  },
 });
 </script>
